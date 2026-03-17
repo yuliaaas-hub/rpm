97ab4943abf5b21d5f01d6a01458b007a1bfbfe1
yuliaaas-hub <ysivokozova@bk.ru>
2026-03-17 13:51:11 +0700

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace task_3._1_rpm
+{
+    public class Square : Figure
+    {
+        public override UIElement CreateUIElement(double size = 50)
+        {
+            return new Rectangle
+            {
+                Width = size,
+                Height = size,
+                Fill = new SolidColorBrush(Color),
+                Margin = new Thickness(5)
+            };
+        }
+    }
+}
