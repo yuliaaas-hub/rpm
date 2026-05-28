cb2453c87fde48093111d13d2f348dc21dffa0f5
yuliaaas-hub <ysivokozova@bk.ru>
2026-05-28 14:04:57 +0700

    Add files via upload

---
+++ lab_11/lab_9/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using lab_9.Serveses;
+using lab_9.ViewModel;
+using lab_9.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lab_9
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //private IServiceProvider _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+
+            // (Singleton)
+            services.AddSingleton<IDialogService, DialogService>();
+            services.AddSingleton<INavigationService, NavigationService>();
+
+            // ViewModels (Transient)
+            services.AddTransient<AboutViewModel>();
+            services.AddTransient<ContactEditViewModel>();
+            services.AddTransient<ContactsListViewModel>();
+
+            // Shell ViewModel (Singleton)
+            services.AddSingleton<MainWindowViewModel>();
+
+            // (Singleton)
+            services.AddSingleton(sp =>
+            {
+                var window = new MainWindow();
+                window.DataContext = sp.GetRequiredService<MainWindowViewModel>();
+                return window;
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            serviceProvider.GetRequiredService<MainWindow>().Show();
+        }
+    }
+
+}
---
+++ lab_11/lab_9/ViewModel/ContactEditViewModel.cs
@@ -0,0 +1,72 @@
+﻿using lab_9.Model;
+using lab_9.Serveses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace lab_9.ViewModel
+{
+    public class ContactEditViewModel : ObservableObject, INavigationAware
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IDialogService _dialogService;
+
+        private Contact _contact = null!;
+
+        private string _editContactName = string.Empty;
+        private string _editContactPhone = string.Empty;
+        public string EditContactName
+        {
+            get => _editContactName;
+            //set { _contact.ContactName = value; OnPropertyChanged(); }
+            set => Set(ref _editContactName, value);
+        }
+        public string EditContactPhone
+        {
+            get => _editContactPhone;
+            //set { _contact.ContactPhoneNum = value; OnPropertyChanged(); }
+            set => Set(ref _editContactPhone, value);
+        }
+        public ICommand SaveCommand { get; }
+        public ICommand CancelCommand { get; }
+        public ContactEditViewModel(IDialogService dialogService, INavigationService navigation)
+        {
+            _dialogService = dialogService;
+            _navigationService = navigation;
+
+            //SaveCommand = new RelayCommand(() => _navigationService.NavigateTo<ContactsListViewModel>());
+            //CancelCommand = new RelayCommand(() => _navigationService.NavigateTo<ContactsListViewModel>());
+            SaveCommand = new RelayCommand(Save, CanSave);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+        public void OnNavigatedTo(object? parameter)
+        {
+            if (parameter is Contact c)
+            {
+                _contact = c;
+                EditContactName = c.Name;
+                EditContactPhone = c.Phone;
+            }
+        }
+        private void Save()
+        {
+            _contact.Name = EditContactName;
+            _contact.Phone = EditContactPhone;
+
+            _dialogService.ShowInfo("Контакт успешно обновлён");
+
+            _navigationService.NavigateTo<ContactsListViewModel>();
+        }
+        private bool CanSave()
+        {
+            return Contact.Validate(EditContactName, EditContactPhone);
+        }
+        private void Cancel()
+        {
+            _navigationService.NavigateTo<ContactsListViewModel>();
+        }
+    }
+}
